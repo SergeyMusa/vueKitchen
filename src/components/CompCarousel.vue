75d1e82962be2a19514eddf114cfc6aab6c30fb3
SergeyMusa <sir.stv@gmail.com>
2022-07-11 23:12:48 +0300

    5.ycarousel

---
+++ src/components/CompCarousel.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+
+        <carousel-3d>
+            <slide :index="0">
+                Slide 1 Content
+            </slide>
+            <slide :index="1">
+                Slide 2 Content
+            </slide>
+            <slide :index="2">
+                Slide 3 Content
+            </slide>
+        </carousel-3d>
+
+        <carousel-3d>
+            <slide v-for="(slide, i) in slides" :index="i" :key="i">
+                <template slot-scope="{ index, isCurrent, leftIndex, rightIndex }">
+                    <img :data-index="index" :class="{ current: isCurrent, onLeft: (leftIndex >= 0), onRight: (rightIndex >= 0) }" :src="slide.src">
+                </template>
+            </slide>
+        </carousel-3d>
+    </div>
+</template>
+
+<script>
+import { Carousel3d, Slide } from 'vue-carousel-3d';
+
+export default {
+//   ...
+  components: {
+    Carousel3d,
+    Slide
+  }
+//   ...
+};
+</script>
---
+++ src/views/ErrorPage.vue
@@ -0,0 +1,63 @@
+<template>
+   <div
+      :style="{
+        'background-image': `url(img/404.jpg)`,
+        width: '99%',
+        height: '88%',
+      }"
+    >
+        <h1>404</h1>
+        <h2>Похоже вы немного заплутали, вернитесь назад</h2>
+            <!-- <img id="scull" alt="My logo" src="img/logo.webp" height="100px"> -->
+
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+/* img/404.jpg */
+body{
+    
+}
+div{
+    background: #cccccc;
+    /* background-image: "img/404.jpg"; */
+           /* background-size: 100% auto; */
+background-size: contain ;
+background-repeat: no-repeat;
+margin: 10%;
+/* background-size:  contain auto; */
+
+ position:absolute;
+  /*  top:0px;
+  right:0px;
+  bottom:0px;
+  left:0px; */
+    /* height: 80%;
+    width: 99%; */
+
+}
+h1 {
+    /* position:absolute; */
+   /*  top:40%;
+    right:50%; */
+    /* margin-top: 20%; */
+    /* text-align: center;
+   
+    display: inline-block; 
+    vertical-align: middle;  */
+
+    /* display: block; */
+
+  /* display: inline-block; */ 
+  margin-top: 13%;
+  vertical-align: middle;
+  
+}
+h2 {
+    line-height: 0.7;
+}
+</style>
