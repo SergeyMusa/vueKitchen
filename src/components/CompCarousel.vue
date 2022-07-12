3625e9928d7236a467bd0a0fd92a195cde36dac0
SergeyMusa <sir.stv@gmail.com>
2022-07-12 12:50:25 +0300

    6.add header & footer

--- src/components/CompCarousel.vue
+++ src/components/CompCarousel.vue
@@ -1,37 +1,50 @@
 <template>
     <div>
-
         <carousel-3d>
             <slide :index="0">
                 Slide 1 Content
+                <img :src=imgUrl alt="My Kitchen">
+                <!-- height="100px" -->
             </slide>
             <slide :index="1">
                 Slide 2 Content
+                {{i}}
             </slide>
             <slide :index="2">
                 Slide 3 Content
+                <!-- {{ii}} -->
             </slide>
         </carousel-3d>
 
-        <carousel-3d>
+        <!-- <carousel-3d>
             <slide v-for="(slide, i) in slides" :index="i" :key="i">
                 <template slot-scope="{ index, isCurrent, leftIndex, rightIndex }">
                     <img :data-index="index" :class="{ current: isCurrent, onLeft: (leftIndex >= 0), onRight: (rightIndex >= 0) }" :src="slide.src">
                 </template>
             </slide>
-        </carousel-3d>
+        </carousel-3d> -->
     </div>
 </template>
 
 <script>
-import { Carousel3d, Slide } from 'vue-carousel-3d';
+import { Carousel3d, Slide, } from 'vue-carousel-3d';
+// import { i } from 'CompCarousel';
 
 export default {
 //   ...
   components: {
     Carousel3d,
     Slide
-  }
-//   ...
+  },
+  data () {
+    return {
+      i: 5,
+      alt: "kitchen",
+      imgUrl: `img/k1/k{{i}}`   
+    }
+  },
+    //  slide: `img/k1/k{{i}}`
+    //   ...
 };
+
 </script>
---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="main">
+    <div class="left">
+      <!-- Логотип -->
+      <img id="scull" alt="My logo" src="img/logo.webp" height="100px" />
+    </div>
+    <div class="right">
+      <a href="tel:78652404050" class="header-phone">📞 + 7 (8652) 404-050</a>
+      <a class="small-button phone-back-call">Заказать звонок</a>
+      <p>Принимаем заявки ежедневно с 9:00 до 18:00</p>
+
+      {картинки соцсетей и адрес}
+      <p>г. Ставрополь, ул. Рогожникова д. 23/2</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SiteHeader",
+  //   props: {
+  //     msg: String
+  //   }
+};
+</script>
+
+<style scoped>
+div.main {
+  /* display: inline-block; */
+  border: 1px solid red;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  /* width: 99%; */
+  margin: 1rem;
+}
+div.left {
+  padding-top: 5px;
+}
+div.right {
+  line-height: 0.4;
+  padding-top: 15px;
+  padding-right: 1rem;
+  text-align: left;
+}
+/* img.scull{
+  height: 120px;
+  width: 220px;
+  padding: 1rem;
+} */
+</style>
