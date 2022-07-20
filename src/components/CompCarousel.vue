9652423e3dd694fa2b416fcbca1934edd1fab24e
SergeyMusa <sir.stv@gmail.com>
2022-07-20 16:56:09 +0300

    add carousel

--- src/components/CompCarousel.vue
+++ src/components/CompCarousel.vue
@@ -1,64 +1,100 @@
 <template>
-    <div>
-       <!-- TODO: -->
+    <div class="slider">
+       <!-- TODO: -->     
+      <hr>
+      <vueper-slides
+        ref="vueperslides1"
+        :touchable="false"
+        fade
+        :autoplay="false"
+        :bullets="false"
+        @slide="$refs.vueperslides2.goToSlide($event.currentSlide.index, { emit: false })"
+        fixed-height="400px"
+        >
+        <vueper-slide
+          v-for="(slide, i) in slides"
+          :key="i"
+          :image="slide.image">
+        </vueper-slide>
+      </vueper-slides>
 
-        <carousel-3d>
-            <slide :index="0">
-                Slide 1 Content
-                <!-- FIXME: -->
-                <!-- <img :src="imgUrl" alt="My Kitchen ${i}"> -->
-                <!-- height="100px" -->
-            </slide>
-            <slide :index="1">
-                Slide 2 Content
-                {{i}}
-            </slide>
-            <slide :index="2">
-                Slide 3 Content
-                <!-- {{ii}} -->
-            </slide>
-        </carousel-3d>
-
-        <!-- <carousel-3d>
-            <slide v-for="(slide, i) in slides" :index="i" :key="i">
-                <template slot-scope="{ index, isCurrent, leftIndex, rightIndex }">
-                    <img :data-index="index" :class="{ current: isCurrent, onLeft: (leftIndex >= 0), onRight: (rightIndex >= 0) }" :src="slide.src">
-                </template>
-            </slide>
-        </carousel-3d> -->
-        <a href="#insta"
+      <vueper-slides
+        class="no-shadow thumbnails"
+        ref="vueperslides2"
+        @slide="$refs.vueperslides1.goToSlide($event.currentSlide.index, { emit: false })"
+        :visible-slides="slides.length"
+        fixed-height="75px"
+        :bullets="false"
+        :touchable="false"
+        :gap="2.5"
+        :arrows="false">
+        <vueper-slide
+          v-for="(slide, i) in slides"
+          :key="i"
+          :image="slide.image"
+          @click.native="$refs.vueperslides2.goToSlide(i)">
+        </vueper-slide>
+      </vueper-slides>
+      <hr>  
+        <!-- <a href="#insta"
           ><img class="socPict" alt="insta" src="../assets/img/k/k1.jpg"
-        /></a>
+        /></a> -->
     </div>
 </template>
 
 <script>
-import { Carousel3d, Slide, } from 'vue-carousel-3d';
-// import { i } from 'CompCarousel';
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
 
-export default {
-  components: {
-    Carousel3d,
-    Slide
-  },
-  data () {
-    return {
-      i: 5,
-    //   alt: "kitchen",
-    // <!-- FIXME: -->
-      // imgUrl: "../assets/img/k/k1.jpg"
-    }
-  },
-  computed:{
-        //   alt: `"kitchen" this.i`,
-    // // imgUrl (){
-    // //    return this.url + ${i} 
-    // // }
-    //     console.log(this.alt);
-
-  },
-    //  slide: `img/k1/k{{i}}`
-    //   ...
-};
-
+  export default {
+    name: "CompCarousel",
+    components: {
+      VueperSlides, VueperSlide
+    },
+    data: () => ({
+    slides: [
+      {
+        title: 'Кухня 23 из дуба',
+      content: 'Стоит 200 тыс.руб.',
+      // You can also provide a URL for the image.
+      image: require('@/assets/img/k/k1.jpg')
+      },
+      {
+        title: 'Кухня 2233 из дуdsfdsfdsf',
+        content: 'Slide 2 content.',
+        image: require('@/assets/img/k/k2.jpg')
+      },
+      {
+        title: 'Кухня 111 из дdfdsfsdffdsf',
+        content: 'Slide 3 content.',
+        image: require('@/assets/img/k/k3.jpg')
+      }
+    ]
+    
+  })
+}
 </script>
+
+<style scoped>
+.slider{
+  width: 75vw;
+}
+.thumbnails {
+  margin: auto;
+  max-width: 300px;
+}
+
+.thumbnails .vueperslide {
+  box-sizing: border-box;
+  border: 1px solid #fff;
+  transition: 0.3s ease-in-out;
+  opacity: 0.7;
+  cursor: pointer;
+}
+
+.thumbnails .vueperslide--active {
+  box-shadow: 0 0 6px rgba(0, 0, 0, 0.3);
+  opacity: 1;
+  border-color: #000;
+}
+</style>
---
+++ src/components/CompSlider.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="slider">
+       <!-- TODO: -->     
+      <hr>
+        <button @click="parallax *= -1;$refs.myVueperSlides.refreshParallax()">
+          reverse parallax effect
+        </button>
+        <button @click="parallaxFixedContent = !parallaxFixedContent">
+          Add a fix title
+        </button>
+
+        <vueper-slides ref="myVueperSlides" :parallax="parallax" :parallax-fixed-content="parallaxFixedContent">
+          <vueper-slide
+            v-for="(slide, i) in slides" :key="i"
+            :image="slide.image"
+            :title="parallaxFixedContent ? slide.title : ''"
+            :content="parallaxFixedContent ? slide.content : ''" />
+        </vueper-slides>
+      <hr>  
+        <!-- <a href="#insta"
+          ><img class="socPict" alt="insta" src="../assets/img/k/k1.jpg"
+        /></a> -->
+    </div>
+</template>
+
+<script>
+import { VueperSlides, VueperSlide } from 'vueperslides'
+import 'vueperslides/dist/vueperslides.css'
+
+  export default {
+    name: "CompCarousel",
+    components: {
+      VueperSlides, VueperSlide
+    },
+    data: () => ({
+    slides: [
+      {
+        title: 'Кухня 23 из дуба',
+      content: 'Стоит 200 тыс.руб.',
+      // You can also provide a URL for the image.
+      image: require('@/assets/img/k/k1.jpg')
+      },
+      {
+        title: 'Кухня 2233 из дуdsfdsfdsf',
+        content: 'Slide 2 content.',
+        image: require('@/assets/img/k/k2.jpg')
+      },
+      {
+        title: 'Кухня 111 из дdfdsfsdffdsf',
+        content: 'Slide 3 content.',
+        image: require('@/assets/img/k/k3.jpg')
+      }
+    ]
+  })
+}
+</script>
+
+<style scoped>
+.slider{
+  width: 75vw;
+}
+</style>
